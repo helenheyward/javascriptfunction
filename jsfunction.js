4e3c3ca29664683f1ece0ee6fa8ab1981e87c994
Helen Heyward <helenheyward@gmail.com>
2015-05-26 12:07:05 +0100

    Update jsfunction.js

--- jsfunction.js
+++ jsfunction.js
@@ -1,5 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html>
+<head>
+<script type="text/javascript">
+
 var foodDemand = function(food)  {
     console.log("I want to eat some " + "" + food);
 };
 
 foodDemand("Toast");
+
+</script>
+</head>
